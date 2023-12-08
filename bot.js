39723444b2d8ca5b1a060070c7091079f9a9839f
bobur2006coder <boburdilmurodov702@gmail.com>
2023-12-08 17:52:17 +0500

    finished bot

---
+++ bot.js
@@ -0,0 +1,6 @@
+const { Telegraf } = require('telegraf');
+var bot = new Telegraf('6846338930:AAFUEb370Evfz39l2YZd7D8S6wl5jbJqVUo');
+bot.start(ctx => ctx.reply(`${ctx}`))
+bot.phone(ctx=>ctx.reply(` nima gap !`))
+
+bot.launch();
